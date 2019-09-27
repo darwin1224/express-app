1c5510bd7b3973625b1676550087f3317caf1f03
Darwin <darwinblacks@gmail.com>
2019-09-27 10:30:26 +0700

    Using class instead method in Authenticate middleware

--- src/middlewares/Authenticate.ts
+++ src/middlewares/Authenticate.ts
@@ -1,11 +1,25 @@
 import * as jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
-export const auth = (req: Request, res: Response, next: NextFunction): any => {
-  try {
-    jwt.verify(req.query.token, '123456');
-    next();
-  } catch (err) {
-    return res.status(401).json({ error: err.message });
-  }
-};
+export class Authenticate {
+  /**
+   * Auth guard
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   * @returns {any}
+   */
+  public static guard = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void | boolean | Response => {
+    try {
+      const token: string = req.get('Authorization')!;
+      return jwt.verify(token, '123456') ? next() : false;
+    } catch (err) {
+      return res.status(401).json({ error: err.message });
+    }
+  };
+}
